1ea5cd48d1bb0df5a23912b342d78b35d44735b9
Avijit Biswas <per.useravi0@gmail.com>
2025-09-13 22:24:19 +0600

    added some variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
+import Announcement from "./components/Announcement";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Newsletter from "./components/Newsletter";
+import ProductBoard from "./components/ProductBoard";
+import { ProductContext } from "./context/context";
+import { initialState, reducer } from "./reducer/ProductReducer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleSearch = (text) => {
+    dispatch({ type: "SEARCH_PRODUCTS", payload: text });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ProductContext.Provider value={{ state, dispatch }}>
+        <Announcement></Announcement>
+        <Header onSearch={handleSearch}></Header>
+        <ProductBoard></ProductBoard>
+        <Newsletter></Newsletter>
+        <Footer></Footer>
+      </ProductContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
