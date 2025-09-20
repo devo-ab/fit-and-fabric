68fe74106c3a5ddae8c0e7addb7bd42aa3ce5b3c
Avijit Biswas <per.useravi0@gmail.com>
2025-09-21 00:25:16 +0600

    fix some issues

--- src/products/ProductList.jsx
+++ src/products/ProductList.jsx
@@ -6,14 +6,14 @@ export default function ProductList({ handleAddToCard, handleRemoveToCart }) {
   const { state } = useContext(ProductContext);
 
   return (
-    <div className="product-grid">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 bg-gradient-to-br from-gray-900 via-black to-gray-900 rounded-lg">
       {state.allProducts.map((product) => (
         <ProductCard
           product={product}
           key={product.id}
           onAdded={handleAddToCard}
           onRemove={handleRemoveToCart}
-        ></ProductCard>
+        />
       ))}
     </div>
   );
